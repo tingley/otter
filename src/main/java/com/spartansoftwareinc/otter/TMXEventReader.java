6903b7d6cc923ae6c96bc738ea98ede6e019281d
Chase Tingley <chase@spartansoftwareinc.com>
2013-08-30 09:59:58 -0700

    Basic state parsing to produce header events, sans details

--- src/main/java/com/spartansoftwareinc/otter/TMXEventReader.java
+++ src/main/java/com/spartansoftwareinc/otter/TMXEventReader.java
@@ -1,10 +1,22 @@
 package com.spartansoftwareinc.otter;
 
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+
+import net.sundell.snax.DefaultElementHandler;
+import net.sundell.snax.NodeModel;
+import net.sundell.snax.NodeModelBuilder;
+import net.sundell.snax.SNAXParser;
+import net.sundell.snax.SNAXUserException;
+
+import static com.spartansoftwareinc.otter.TMXEventType.*;
 
 public class TMXEventReader {
     
@@ -15,16 +27,121 @@ public class TMXEventReader {
 
     private TMXEventReader(Reader r) throws XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newFactory();
-        this.reader = factory.createXMLEventReader(r);
+        parser = SNAXParser.createParser(factory, buildModel());
+        parser.startParsing(r, new TMXEventSink());
     }
     
-    private XMLEventReader reader;
+    private SNAXParser<TMXEventSink> parser;
+    private List<TMXEvent> events = new ArrayList<TMXEvent>();
 
-    public boolean hasNext() {
-        throw new UnsupportedOperationException();
+    public boolean hasNext() throws XMLStreamException {
+        if (events.size() > 0) {
+            return true;
+        }
+        while (parser.hasMoreEvents()) {
+            parser.processEvent();
+            if (events.size() > 0) {
+                return true;
+            }
+        }
+        return false;
     }
     
     public TMXEvent nextEvent() {
-        throw new UnsupportedOperationException();
+        if (events.size() > 0) {
+            return events.remove(0);
+        }
+        throw new IllegalStateException("No event available");
+    }
+    
+    class TMXEventSink {
+        
+    }
+    
+    protected void addEvent(TMXEvent event) {
+        events.add(event);
+    }
+    
+    private NodeModel<TMXEventSink> buildModel() {
+        return new NodeModelBuilder<TMXEventSink>() {{
+            elements("tmx").attach(new TMXHandler());
+            elements("tmx", "header").attach(new HeaderHandler());
+            elements("tmx", "header", "prop").attach(new PropertyHandler());
+            elements("tmx", "header", "note").attach(new NoteHandler());
+            elements("tmx", "body").attach(new BodyHandler());
+        }}.build();
+    }
+    
+    class TMXHandler extends DefaultElementHandler<TMXEventSink> {
+        @Override
+        public void startElement(StartElement element, TMXEventSink data)
+                throws SNAXUserException {
+            // TODO verify version
+            addEvent(new TMXEvent(START_TMX));
+        }
+        @Override
+        public void endElement(EndElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(END_TMX));
+        }
+    }
+    class HeaderHandler extends DefaultElementHandler<TMXEventSink> {
+        @Override
+        public void startElement(StartElement element, TMXEventSink data)
+                throws SNAXUserException {
+            // TODO Handle attributes
+            addEvent(new TMXEvent(START_HEADER));
+        }
+        @Override
+        public void endElement(EndElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(END_HEADER));
+        }
+    }
+    class PropertyHandler extends DefaultElementHandler<TMXEventSink> {
+        @Override
+        public void startElement(StartElement element, TMXEventSink data)
+                throws SNAXUserException {
+            // TODO Handle attributes, start buffering
+        }
+        @Override
+        public void characters(StartElement parent, Characters characters,
+                TMXEventSink data) throws SNAXUserException {
+            // TODO handle
+        }
+        @Override
+        public void endElement(EndElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(PROPERTY));
+        }
+    }
+    class NoteHandler extends DefaultElementHandler<TMXEventSink> {
+        @Override
+        public void startElement(StartElement element, TMXEventSink data)
+                throws SNAXUserException {
+            // TODO Handle attributes, start buffering
+        }
+        @Override
+        public void characters(StartElement parent, Characters characters,
+                TMXEventSink data) throws SNAXUserException {
+            // TODO handle
+        }
+        @Override
+        public void endElement(EndElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(NOTE));
+        }
+    }
+    class BodyHandler extends DefaultElementHandler<TMXEventSink> {
+        @Override
+        public void startElement(StartElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(START_BODY));
+        }
+        @Override
+        public void endElement(EndElement element, TMXEventSink data)
+                throws SNAXUserException {
+            addEvent(new TMXEvent(END_BODY));
+        }
     }
 }
